5ab97a83d42383393d23eafcdaa3dc463ff12bf6
zhaolei <zhaolei@allinmd.cn>
2016-02-26 20:26:03 +0800

    SubmoduleMMM init

---
+++ SubmoduleMMM/SubmoduleMMM.h
@@ -0,0 +1,13 @@
+//
+//  SubmoduleMMM.h
+//  SubmoduleMMM
+//
+//  Created by zhaolei on 16/2/26.
+//  Copyright © 2016年 zhaolei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SubmoduleMMM : NSObject
+
+@end
---
+++ SubmoduleMMM/SubmoduleMMM.m
@@ -0,0 +1,13 @@
+//
+//  SubmoduleMMM.m
+//  SubmoduleMMM
+//
+//  Created by zhaolei on 16/2/26.
+//  Copyright © 2016年 zhaolei. All rights reserved.
+//
+
+#import "SubmoduleMMM.h"
+
+@implementation SubmoduleMMM
+
+@end
